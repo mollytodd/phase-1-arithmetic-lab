3cf4e0ead6b851ac8c3fd68cd46d06408980165b
Molly Todd <mollyjt89@gmail.com>
2023-06-20 18:50:13 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 
-// Write your code here
+let multiply = 62
+
+let num1 = multiply / num2
+
+let num2 = multiply / num1
+
+// this seems like a sloppy way of doing this lol
+
+let random = Math.floor(Math.random() * 10) + 0;
+// is this the best way to do this???? 
+
+//mathrandom() returns a random number between 0 and .99. which 
+//is why we multiply by 10
+// we add math floor to round to nearest whole number
+
+let mod = 4
+
+let num3 = multiply % mod
+
+let num4 = multiply % mod
+
+// tab9 helped me figure this out. need to figure out how to do this. here is some definitions
+
+//Division: It divides the left operand by the right operand.
+//Remainder: It returns the remainder obtained from the division.
+//Here's an example to illustrate how the remainder operator works:
+
+//javascript
+///opy code
+//let dividend = 17;
+//let divisor = 5;
+
+//let remainder = dividend % divisor;
+//console.log(remainder);  // Output: 2
+//In this example, the dividend is 17, and the divisor is 5. 
+//When we perform the division operation 17 / 5, the quotient i
+//s 3 with a remainder of 2. The remainder operator % returns the remainder, 
+//which is 2 in this case.
+
+let max = 20
+
+let min = 0
+
+Math.max()/Math.min()
+
+//Create a variable called max that finds the highest number in a set; the value returned should be 20.
